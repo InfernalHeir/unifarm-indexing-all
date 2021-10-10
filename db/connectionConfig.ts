8af9b1730503afa6ff0a4434245d2d2f8dacb224
Bhupendra Bisht <bhupendrabisht61049@gmail.com>
2021-10-10 11:57:25 +0530

    docker file and connection changes

--- db/connectionConfig.ts
+++ db/connectionConfig.ts
@@ -1,14 +1,14 @@
 import dotenv from "dotenv";
 import { ConnectionOptions } from "typeorm";
-import { Claim } from "./entity/Claim";
-import { Cohort } from "./entity/Cohort";
-import { RefferralClaim } from "./entity/RefferalClaim";
-import { Stake } from "./entity/Stake";
-import { Token } from "./entity/Token";
-import { Unstake } from "./entity/Unstake";
 
 // config
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+var entities = "./db/entity/*.ts";
+
+if (process.env.NODE_ENV === "prod") {
+   entities = "./build/db/entity/*.js";
+}
 
 // client options
 type ClientOptions = ConnectionOptions;
@@ -22,11 +22,7 @@ export const clientOps: ClientOptions = {
    database: String(process.env.DB_NAME),
    synchronize: true,
    logging: false,
-   entities: [Cohort, Token, Stake, Unstake, Claim, RefferralClaim],
+   entities: [entities],
    logNotifications: true,
    name: "unifarm",
-   extra: {
-      connectionLimits: 5,
-      keepConnectionAlive: true,
-   },
 };
