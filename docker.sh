488e0c1c4ff714192480ea0a330c3b5036e4ab03
Bhupendra Bisht <bhupendrabisht61049@gmail.com>
2021-11-25 17:59:19 +0530

    docker script changes

--- docker.sh
+++ docker.sh
@@ -6,8 +6,8 @@ export REDIS_PASSWORD=TdePagKTvyhAcnYR
 
 echo "Creating the Graph ql server image"
 
-docker image build -t graph:2.0.2 -f ./graphql/Dockerfile .
+docker image build --no-cache -t graph:2.0.2 -f ./graphql/Dockerfile .
 
 echo "Creating the Unifarm Event Listeners image"
 
-docker image build -t events-listener:4.4.4 -f ./events/Dockerfile .
+docker image build --no-cache -t events-listener:4.4.4 -f ./events/Dockerfile .
